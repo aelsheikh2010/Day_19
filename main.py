97eefbbc49baa8cbc99aff2b0edbde6af77a0d1f
Ahmed Elsheikh <golf17@gmail.com>
2023-01-24 20:26:50 +0200

    turtle game commit

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
+import turtle
 from turtle import Turtle, Screen
-
-
+import random
+is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="which turtle will win the race? Enter a color")
 colors = ["red", "blue", "orange", "yellow", "green", "purple"]
 y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
+#crate a multiple turtle and insert them in all_turtles list
 for turtle_index in range(0, 6):
-    tim = Turtle(shape="turtle")
-    tim.color(colors[turtle_index])
-    tim.penup()
-    tim.goto(x=-230, y=y_positions[turtle_index])
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
 
 
+if user_bet:
+    is_race_on = True
+while is_race_on:
+    for turtle in all_turtles:
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
 
 
 screen.exitonclick()
